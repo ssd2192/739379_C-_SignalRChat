7b67e19461f9f5851dd81ed469d1889420e09777
Sandeep <ssd2192@gmail.com>
2019-07-17 11:56:08 -0400

    A-A

---
+++ 739379_17_7_19/Scripts/myScript.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $(’#container’).hide();
+});
+function startDiscussion() {
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatHub;
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        var encodedName = $(’<div />’).text(name).html();
+        var encodedMsg = $(’<div />’).text(message).html();
+        $(’#discussion’).append(’<li><h4>’ + encodedName
++ ’</h4>: ’ + encodedMsg + ’</li>’);
+    };
+    $(’#message’).focus();
+    $.connection.hub.start().done(function () {
+        $(’#sendmessage’).click(function () {
+            chat.server.send($(’#displayname’).val(), $(’#message’).val());
+            $(’#message’).val(’’).focus();
+        });
+    });
+}
+4
+function AddUser() {
+    if ($(’#username’).val() == "") {
+        alert(’enter username’);
+        return;
+    }
+    $(’#displayname’).val($(’#username’).val());
+    $(’#container’).show();
+    $(’#setUser’).hide();
+    startDiscussion();
+    $(’#welcome’).append(" : " + $(’#username’).val());
+}
---
+++ 739379_17_7_19/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _739379_17_7_19
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
